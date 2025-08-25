3ceba737f57bcbc84c5228fb2da88c1557e12c9c
FeriusDMS <91622801+FeriusDMS@users.noreply.github.com>
2025-08-25 19:31:45 +0200

    FIX(Action): add the initial missing files to 'help' the release

---
+++ HPTarget/HPTarget.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HPTarget
+{
+    public class Plugin
+    {
+        public void Initialize()
+        {
+            Console.WriteLine("HPTarget Plugin loaded.");
+        }
+    }
+}
