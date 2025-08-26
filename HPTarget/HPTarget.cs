e6e90097e6b5a7f0304c98f20800b8dcbdd3f69b
FeriusDMS <91622801+FeriusDMS@users.noreply.github.com>
2025-08-27 00:02:40 +0200

    build(api13): WIP back the 9.0 windows version for Dalamud and add the using IoC

--- HPTarget/HPTarget.cs
+++ HPTarget/HPTarget.cs
@@ -4,6 +4,7 @@ using Dalamud.Interface.Windowing;
 using Dalamud.Bindings.ImGui;
 using Dalamud.Game.ClientState.Objects.SubKinds;
 using Dalamud.Game.ClientState.Objects.Types;
+using Dalamud.IoC;
 
 namespace HPTarget;
 
