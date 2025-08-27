922c0db3fbc56f05c1616807d15fbc7f7d658239
FeriusDMS <91622801+FeriusDMS@users.noreply.github.com>
2025-08-27 19:32:48 +0200

    build(api13): add new debug log

--- HPTarget/HPTarget.cs
+++ HPTarget/HPTarget.cs
@@ -20,11 +20,17 @@ public class Plugin : IDalamudPlugin {
     private readonly HPWindow window = new();
 
     public Plugin() {
+        Log.Info("Do i go here?");
+        Log.Information("Do i go here2?");
         ws.AddWindow(window);
         PluginInterface.UiBuilder.Draw += Draw;
     }
 
-    private void Draw() => ws.Draw();
+    private void Draw() {
+        Log.Info("Do i draw?");
+        Log.Information("Do i draw2?");
+        ws.Draw();
+    }
 
     public void Dispose() {
         ws.RemoveAllWindows();
@@ -36,6 +42,8 @@ public class HPWindow : Window {
     public HPWindow() : base("HPTarget Overlay", ImGuiWindowFlags.AlwaysAutoResize) {}
 
     public override void Draw() {
+        Plugin.Log.Info("So not here");
+        Plugin.Log.Information("So not here2");
         var target = Plugin.TargetManager.Target as IBattleChara;
         if (target == null) return;
         ImGui.Text($"{target.CurrentHp:n0} / {target.MaxHp:n0}");
